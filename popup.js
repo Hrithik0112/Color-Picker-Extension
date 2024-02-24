f7c86d0564d5bcc41717f2bd44987f36f29bd49b
Hrithik <slumdog4321@gmail.com>
2024-02-24 19:56:01 +0530

    script inject to the current webpage

--- popup.js
+++ popup.js
@@ -1,5 +1,24 @@
 const btn = document.querySelector(".changecolorBtn");
 
-btn.addEventListener("click", () => {
-  console.log("Clicked on button captured");
+btn.addEventListener("click", async () => {
+  // Query for active tabs
+  let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  // Check if any tabs are found
+  if (tabs.length > 0) {
+    // Use the first tab found
+    let tab = tabs[0];
+
+    // Execute the script on the tab
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: snatchColor,
+    });
+  } else {
+    console.error("No active tabs found.");
+  }
 });
+
+function snatchColor() {
+  console.log("Script Working");
+}
